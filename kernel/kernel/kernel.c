dfbf2bf14d62fd23722ab5373f554d4fa2c05de7
RMidhunSuresh <rmidhunsuresh@gmail.com>
2020-10-31 18:01:43 +0530

    Refactored print statements into function

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -2,10 +2,13 @@
 
 #include <kernel/tty.h>
 
-void kernel_main(void) {
-	terminal_initialize();
+void print_header(){
+    terminal_initialize();
     terminal_write_string_centered("DAX Operating System");
     terminal_write_string_centered("Build 1.0 2020 (c)");
     terminal_print_horizontal_rule('-');
-	//printf("DAX Operating System\nBuild 1.0 2020 (c)");
+}
+
+void kernel_main(void) {
+    print_header();
 }
