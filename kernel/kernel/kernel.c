7f391d9ceb135fc5dae991433549618370c88266
RMidhunSuresh <rmidhunsuresh@gmail.com>
2020-11-05 19:34:01 +0530

    Multiple changes:
    - Changed terminal color to white text on black background
    - Added helpful comment to Makefile
    - Refactored some code

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -2,13 +2,15 @@
 
 #include <kernel/tty.h>
 
-void print_header(){
-    terminal_initialize();
+void print_header()
+{
     terminal_write_string_centered("DAX Operating System");
     terminal_write_string_centered("Build 1.0 2020 (c)");
     terminal_print_horizontal_rule('-');
 }
 
-void kernel_main(void) {
+void kernel_main(void)
+{
+    terminal_initialize();
     print_header();
 }
