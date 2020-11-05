74bb5b0e08ffd28f6e30b092b58285820fd6dd59
RMidhunSuresh <rmidhunsuresh@gmail.com>
2020-11-05 19:47:10 +0530

    Renamed myos to DaxOS

--- iso.sh
+++ iso.sh
@@ -6,10 +6,10 @@ mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 
-cp sysroot/boot/myos.kernel isodir/boot/myos.kernel
+cp sysroot/boot/DaxOS.kernel isodir/boot/DaxOS.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
-menuentry "myos" {
-	multiboot /boot/myos.kernel
+menuentry "DaxOS"{
+	multiboot /boot/DaxOS.kernel
 }
 EOF
-grub-mkrescue -o myos.iso isodir
+grub-mkrescue -o DaxOS.iso isodir
