c61e35cb73f600f596d2675bcb6b772eb6b69e93
RMidhunSuresh <rmidhunsuresh@gmail.com>
2021-03-19 11:11:44 +0530

    Removed assert.h

--- libc/include/assert.h
+++
@@ -1,9 +0,0 @@
-#ifndef DAXOS_ASSERT_H
-#define DAXOS_ASSERT_H
-#include <stdio.h>
-
-#define assert(test) \
-  ((test) \
-    ? (print_f("assertion failed, " __FILE__ ":" __LINE__ ": " #test), /*panic()*/) \
-    : 0)
-#endif //DAXOS_ASSERT_H
