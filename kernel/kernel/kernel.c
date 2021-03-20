99d27d868a62d4af90e46f458707eee4ee24eb5f
RMidhunSuresh <rmidhunsuresh@gmail.com>
2021-03-20 14:57:42 +0530

    Get multiboot information from GRUB bootloader

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -6,9 +6,12 @@
 #include <kernel/interrupts.h>
 #include <kernel/gdt.h>
 #include <stdlib.h>
+#include <kernel/multiboot_32.h>
 
 // Comment out the follow macro definiton to disable unit tests from running
 #define D_UNIT_ENABLED
+
+
 
 void print_header()
 {
@@ -29,7 +32,7 @@ void init_descriptors()
     idt_init();
 }
 
-void kernel_main(void)
+void kernel_main(multiboot_info_t* mbt, unsigned int magic)
 {
     init_descriptors();
     init_devices();
@@ -37,4 +40,16 @@ void kernel_main(void)
 #ifdef D_UNIT_ENABLED
     D_UNIT_run();
 #endif
+    mmap_entry_t* entry = mbt->mmap_addr;
+    int i=0; int x;
+	while(entry < mbt->mmap_addr + mbt->mmap_length) {
+		// do something with the entry
+		entry = (mmap_entry_t*) ((unsigned int) entry + entry->size + sizeof(entry->size));
+        printf("Entry %d\n", i+1);
+        printf("size = %d\n", entry->size);
+        printf("Base address low = %d, high = %d\n", entry->base_addr_low, entry->base_addr_high);
+        printf("Length low = %d, high = %d\n", entry->length_low, entry->length_high);
+        printf("Type = %d\n", entry->type);
+        scanf("%d", &x);
+	}
 }
