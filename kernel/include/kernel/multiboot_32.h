5e93368c206585dde4c6cf962ba99fa6e6cfa802
RMidhunSuresh <rmidhunsuresh@gmail.com>
2021-03-23 16:10:12 +0530

    Replaced int with 32-bit uints from stdint.h
    - There is no guarantee that an int is 4 bytes.

--- kernel/include/kernel/multiboot_32.h
+++ kernel/include/kernel/multiboot_32.h
@@ -2,17 +2,18 @@
 #define MULTIBOOT_EXTRA_H
 
 #include <kernel/multiboot.h>
+#include <stdint.h>
 
 typedef struct multiboot_memory_map {
-	unsigned int size;
-	unsigned int base_addr_low,base_addr_high;
-	unsigned int length_low,length_high;
+	uint32_t size;
+	uint32_t base_addr_low,base_addr_high;
+	uint32_t length_low,length_high;
 	#define MULTIBOOT_MEMORY_AVAILABLE              1
 	#define MULTIBOOT_MEMORY_RESERVED               2
 	#define MULTIBOOT_MEMORY_ACPI_RECLAIMABLE       3
 	#define MULTIBOOT_MEMORY_NVS                    4
 	#define MULTIBOOT_MEMORY_BADRAM                 5
-	unsigned int type;
+	uint32_t type;
 } multiboot_memory_map_t;
  
 // this is really an entry, not the entire map.
