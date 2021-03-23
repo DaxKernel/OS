b8d80fff9a001da7b2cdb4abae2b49add7a89682
RMidhunSuresh <rmidhunsuresh@gmail.com>
2021-03-23 20:48:01 +0530

    Reimplement physical memory allocator
    - Removed free_list struct; grub mmap is enough.
    - Made global variables static
    - Replaced native int with fixed 32-bit ints

--- kernel/arch/i386/mem/mem.c
+++ kernel/arch/i386/mem/mem.c
@@ -2,111 +2,86 @@
 #include <kernel/mem/p_stack.h>
 #include <stdio.h>
 
+static multiboot_info_t* mbt;
+
 extern char _kernel_end;
 
 // Size of each page frame in KB
-const int frame_size = 4 * 1024;
+static const int frame_size = 4 * 1024;
 
-typedef struct {
-  // Start of free block of memory
-  void *start;
-  // Length of this block
-  unsigned int length;
-} free_mem;
+static const uint32_t MB = 1024 * 1024;
 
-int num_free_mem;
-free_mem free_mem_list[10];
+static int stack_size = -1;
 
-// Get the size of the stack needed in bytes
-int get_stack_size(){
-  int sum=0;
-  for(int i=0; i<num_free_mem; ++i){
-    sum += free_mem_list[i].length;
-  }
-  return sum/frame_size;
-}
-
-void print(int i){
-  printf("start = %d\n", free_mem_list[i].start);
-  printf("length = %d\n\n", free_mem_list[i].length);
-}
-
-void correct_mem(){
-  for(int i=0; i<num_free_mem; ++i){
-    if(free_mem_list[i].start < &_kernel_end){
-
-      // Calculations for adjusting kernel in memory
-      void *first_start = &_kernel_end;
-      int first_length = free_mem_list[i].length - (int)((char *)first_start - (char *)free_mem_list[i].start);
-
-      // Calculations for adjusting stack in memory
-      void *second_start = (int *)first_start + (first_length / frame_size);
-      int second_length = first_length - (int)((char *)second_start - (char *)first_start);
-
-      print(i);
-      free_mem_list[i].start = second_start;
-      free_mem_list[i].length = second_length;
-      print(i);
-    }
-  }
-}
-
-void allocate_buffer(){
-  correct_mem();
-  printf("End of kernel is at %d\n", &_kernel_end);
-  int stack_size = get_stack_size();
-  printf("Stack size = %d\n", stack_size);
+void allocate_buffer(){  
   init_stack(stack_size);
-  for(int i=0; i<num_free_mem; ++i){
-    // Push free memory addresses in blocks of 4kb into stack
-    const int num_frames = free_mem_list[i].length / frame_size;
-    printf("Num frames = %d\n", num_frames);
-    for(int j=0; j<num_frames; ++j){
-      push((char *)free_mem_list[i].start + j*frame_size);
+  mmap_entry_t* entry = (mmap_entry_t *)mbt->mmap_addr;
+  while(entry < mbt->mmap_addr + mbt->mmap_length) {
+    if(entry->type == MULTIBOOT_MEMORY_AVAILABLE && entry->base_addr_low >= MB){
+      for(int j=0; j<stack_size; ++j)
+        push((char *)entry->base_addr_low + j*frame_size);
     }
+    entry = (mmap_entry_t*) ((uint32_t) entry + entry->size + sizeof(entry->size));
   }
 }
+
+
 
 void parse_available_mem(multiboot_info_t* mbt){
   mmap_entry_t* entry = (mmap_entry_t *)mbt->mmap_addr;
-  int i=0;
+  uint32_t total_bytes = 0;
 	while(entry < mbt->mmap_addr + mbt->mmap_length) {
-		entry = (mmap_entry_t*) ((unsigned int) entry + entry->size + sizeof(entry->size));
-    if(entry->type == MULTIBOOT_MEMORY_AVAILABLE){
-      // Found a free block of memory; lets parse it.
-      free_mem_list[i].start = (void *)entry->base_addr_low;
-      free_mem_list[i].length = entry->length_low;
-      i++;
+    /* Does a free block of memory overlap with the region in memory
+         where we have the kernel and stack stored? 
+         If so, lets mutate the struct so that the free region starts from 
+         the end of the stack.
+      */
+    if(entry->type == MULTIBOOT_MEMORY_AVAILABLE && entry->base_addr_low >= MB){
+      
+      if(entry->base_addr_low < &_kernel_end){
+        // Calculations for adjusting kernel in memory.
+        void *first_start = &_kernel_end;
+        uint32_t first_length = entry->length_low - (uint32_t)((char *)first_start - (char *)entry->base_addr_low);
+
+        // Calculations for adjusting stack in memory.
+        void *second_start = (uint32_t *)first_start + (first_length / frame_size);
+        uint32_t second_length = first_length - (uint32_t)((char *)second_start - (char *)first_start);
+
+        // TODO: This isn't precisely correct. We are kind of overallocating on the stack.
+        entry->base_addr_low = (uint32_t)second_start;     
+        entry->length_low = second_length;
+    
+      }
+      total_bytes += entry->length_low;
     }
+		entry = (mmap_entry_t*) ((uint32_t) entry + entry->size + sizeof(entry->size));
 	}
-  num_free_mem = i;
+  stack_size = total_bytes / frame_size;
 }
 
-void print_free_list(){
-  for(int i=0; i<num_free_mem; ++i){
-    printf("Free block starts at %d\n", (int *)free_mem_list[i].start);
-    printf("Length is %d\n", free_mem_list[i].length);
-  }
-}
 
-void print_grub_mmap(multiboot_info_t* mbt){
+void print_grub_mmap(){
   mmap_entry_t* entry = (mmap_entry_t *)mbt->mmap_addr;
   int i=0, x;
 	while(entry < mbt->mmap_addr + mbt->mmap_length) {
-		entry = (mmap_entry_t*) ((unsigned int) entry + entry->size + sizeof(entry->size));
         printf("Mem location #%d\n", ++i);
         printf("base_low = %d base_high=%d\n", entry->base_addr_low, entry->base_addr_high);
         printf("length_low = %d, length_high = %d\n", entry->length_low, entry->length_high);
         printf("Type = %d\n\n", entry->type);
         scanf("%d", &x);
+        entry = (mmap_entry_t*) ((uint32_t) entry + entry->size + sizeof(entry->size));
 	}
 }
 
-int* get_page(){
-  return (int *)pop();
+void set_multiboot_info_t(multiboot_info_t* m){
+  mbt = m;
 }
 
-void free_page(int *page){
-  push((int)page);
+uint32_t* get_page(){
+  return (uint32_t *)pop();
+}
+
+void free_page(uint32_t *page){
+  push((uint32_t)page);
 }
 
--- kernel/include/kernel/mem/mem.h
+++ kernel/include/kernel/mem/mem.h
@@ -1,8 +1,9 @@
 #ifndef MEM_H
 #define MEM_H
     #include <kernel/multiboot_32.h>
+    void set_multiboot_info_t(multiboot_info_t* m);
     void allocate_buffer();
-    void parse_available_mem(multiboot_info_t* mbt);
-    void print_free_list();
-    int* get_page();
+    void parse_available_mem();
+    uint32_t* get_page();
+    void free_page(uint32_t *page);
 #endif
