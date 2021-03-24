c1c7f8b151a46b457e7e0069feb41b9979bd47f2
RMidhunSuresh <rmidhunsuresh@gmail.com>
2021-03-24 15:42:49 +0530

    Fix indent

--- kernel/include/kernel/mem/mem.h
+++ kernel/include/kernel/mem/mem.h
@@ -1,11 +1,13 @@
 #ifndef MEM_H
 #define MEM_H
-    #include <kernel/multiboot_32.h>
-    #include <stddef.h>
-    
-    void allocate_buffer();
-    void parse_available_mem();
-    uint32_t* get_page();
-    void free_page(uint32_t *page);
-    size_t get_num_free_pages();
+
+#include <kernel/multiboot_32.h>
+#include <stddef.h>
+
+void allocate_buffer();
+void parse_available_mem();
+uint32_t* get_page();
+void free_page(uint32_t *page);
+size_t get_num_free_pages();
+
 #endif
--- kernel/include/kernel/mem/mmap.h
+++ kernel/include/kernel/mem/mmap.h
@@ -1,7 +1,9 @@
 #ifndef MMAP_H
 #define MMAP_H
-    #include <kernel/multiboot_32.h>
-    extern multiboot_info_t* mbt;
-    void mmap_iterate(void (*)(mmap_entry_t*));
-    void set_multiboot_info_t(multiboot_info_t* m);
+
+#include <kernel/multiboot_32.h>
+extern multiboot_info_t* mbt;
+void mmap_iterate(void (*)(mmap_entry_t*));
+void set_multiboot_info_t(multiboot_info_t* m);
+
 #endif
