e27026b3f08075597971f7aca36dcf2d686ce39a
RMidhunSuresh <rmidhunsuresh@gmail.com>
2021-04-16 19:43:32 +0530

    Fixed uint overflow bug in liballoc

--- kernel/include/kernel/mem/liballoc.h
+++ kernel/include/kernel/mem/liballoc.h
@@ -7,15 +7,13 @@
 // Hooks needed; refer to hooks.c file for more information.
 extern int liballoc_lock();
 extern int liballoc_unlock();
-extern void* liballoc_alloc(size_t);
-extern int liballoc_free(void*,size_t);
+extern void *liballoc_alloc(size_t);
+extern int liballoc_free(void *, size_t);
 
 // Our functions
-extern void    *malloc(size_t);
-extern void    *realloc(void *, size_t);
-extern void    *calloc(size_t, size_t);
-extern void     free(void *);
+extern void *malloc(size_t);
+extern void *realloc(void *, size_t);
+extern void *calloc(size_t, size_t);
+extern void free(void *);
 
 #endif
-
-
