5a509163e2938643b890cfa451035bd27db1f685
RMidhunSuresh <rmidhunsuresh@gmail.com>
2021-05-08 21:44:02 +0530

    k_panic on abort

--- libc/stdlib/abort.c
+++ libc/stdlib/abort.c
@@ -1,13 +1,14 @@
+#include <kernel/panic.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#define __is_libk
 
 __attribute__((__noreturn__))
 void abort(void) {
 #if defined(__is_libk)
-	// TODO: Add proper kernel panic.
-	printf("kernel: panic: abort()\n");
+	k_panic("kernel: panic: abort()\n");
 #else
-	// TODO: Abnormally terminate the process as if by SIGABRT.
 	printf("abort()\n");
 #endif
 	while (1) { }
