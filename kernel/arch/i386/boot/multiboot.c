df394e22dcc408b379bd8a351c8bc66eef5ffcd9
RMidhunSuresh <rmidhunsuresh@gmail.com>
2021-05-08 21:42:00 +0530

    k_panic on multiboot verification failure

--- kernel/arch/i386/boot/multiboot.c
+++ kernel/arch/i386/boot/multiboot.c
@@ -1,18 +1,17 @@
 #include <kernel/multiboot_32.h>
 #include <kernel/multiboot.h>
+#include <kernel/panic.h>
 #include <stdio.h>
 
 inline void verify_multiboot_header(uint32_t magic){
     if(magic != MULTIBOOT_BOOTLOADER_MAGIC){
-        printf("Invalid Bootloader\n");
-        //kernel_panic
+        k_panic("Invalid Bootloader\n");
     }
 }
 
 inline void verify_multiboot_mmap(multiboot_info_t* mbt){
   if((mbt->flags & (1<<6)) == 0){
-      printf("Could not get memory map from Multiboot.\n");
-      //kernel_panic
+      k_panic("Could not get memory map from Multiboot.\n");
   }
 }
 
