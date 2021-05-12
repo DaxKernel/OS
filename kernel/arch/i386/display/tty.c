f1a559d795c743d0140dc1fb9bdc99696295ccc8
RMidhunSuresh <rmidhunsuresh@gmail.com>
2021-05-12 13:47:34 +0530

    Remove vga.h since it is no longer needed

--- kernel/arch/i386/display/tty.c
+++ kernel/arch/i386/display/tty.c
@@ -4,34 +4,9 @@
 #include <string.h>
 #include <kernel/tty.h>
 #include <kernel/panic.h>
-#include "vga.h"
 
 #define SSFN_CONSOLEBITMAP_TRUECOLOR
-#define _STDINT_H
 #include <kernel/ssfn.h>
-
-static const size_t VGA_WIDTH = 80;
-static const size_t VGA_HEIGHT = 25;
-
-static size_t terminal_row;
-static size_t terminal_column;
-static uint8_t terminal_color;
-static uint16_t *terminal_buffer;
-
-void tty_clear_row(size_t row)
-{
-    for (size_t x = 0; x < VGA_WIDTH; x++)
-    {
-        const size_t index = row * VGA_WIDTH + x;
-        terminal_buffer[index] = vga_entry(' ', terminal_color);
-    }
-}
-
-void tty_clear()
-{
-    for (size_t y = 0; y < VGA_HEIGHT; y++)
-        tty_clear_row(y);
-}
 
 bool verify_mbt_framebuffer(multiboot_info_t *mbt)
 {
@@ -55,11 +30,7 @@ void tty_initialize(multiboot_info_t *mbt)
     ssfn_dst.bg = 0xFF000000;
     ssfn_dst.x = 0;
     ssfn_dst.y = 0;
-}
-
-static inline void tty_newline()
-{
-    ssfn_putc('\n');
+    tty_print_success("VESA Graphics Driver", "OK");
 }
 
 void tty_insert_char(char c)
@@ -69,40 +40,31 @@ void tty_insert_char(char c)
 
 void tty_push_text_upward()
 {
-    size_t y;
-    for (y = 0; y < VGA_HEIGHT - 1; y++)
-    {
-        for (size_t x = 0; x < VGA_WIDTH; x++)
-        {
-            const size_t index = y * VGA_WIDTH + x;
-            terminal_buffer[index] = terminal_buffer[index + VGA_WIDTH];
-        }
-    }
-    tty_clear_row(y);
 }
 
 void tty_backspace()
 {
-    ssfn_dst.x -= x_skip;
+    const uint8_t char_width = ssfn_src->width / 2;
+    ssfn_dst.x -= char_width;
     ssfn_putc(' ');
-    ssfn_dst.x -= x_skip;
+    ssfn_dst.x -= char_width;
 }
 
 void tty_put_char(char c)
 {
     if (c == '\n')
-        tty_newline();
+        ssfn_putc('\n');
     else if (c == '\b')
         tty_backspace();
     else
         tty_insert_char(c);
-    if (terminal_row >= VGA_HEIGHT)
+    /*     if (terminal_row >= VGA_HEIGHT)
     {
         // At this point screen is full of text
         // Push rows upward
         tty_push_text_upward();
         terminal_row--;
-    }
+    } */
 }
 
 void tty_write(const char *data, size_t size)
@@ -116,15 +78,13 @@ void tty_write_string(const char *data)
     tty_write(data, strlen(data));
 }
 
-void tty_print_horizontal_rule(const char symbol)
+void tty_print_horizontal_rule(/* const char symbol */)
 {
-    if (terminal_column != 0)
-        tty_newline();
-    for (size_t i = 0; i < VGA_WIDTH; ++i)
+    /*     for (size_t i = 0; i < VGA_WIDTH; ++i)
     {
         tty_put_char(symbol);
     }
-    tty_newline();
+    tty_newline(); */
 }
 
 void tty_print_success(const char *string, const char *success_string)
