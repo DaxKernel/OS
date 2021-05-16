3f244622ada4a370b3eea3783f8b332d7637c0c7
RMidhunSuresh <rmidhunsuresh@gmail.com>
2021-05-16 21:14:20 +0530

    Global info for all

---
+++ kernel/include/kernel/display/common.h
@@ -0,0 +1,29 @@
+#include <stdint.h>
+
+typedef struct
+{
+    void *framebuffer;
+    uint16_t width;
+    uint16_t height;
+    uint16_t pitch;
+} vbe_info_t;
+
+/*
+Recently drawn (x, y) pixel position.
+Useful to select a starting position to draw something.
+*/
+typedef struct
+{
+    uint16_t x;
+    uint16_t y;
+} position_info_t;
+
+typedef struct
+{
+    uint32_t fg;
+    uint32_t bg;
+} color_info_t;
+
+extern vbe_info_t vbe_info;
+extern position_info_t position_info;
+extern color_info_t color_info;
