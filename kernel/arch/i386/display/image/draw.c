cd738ce734c3b9668c3dedc70afa14c607093cb5
RMidhunSuresh <rmidhunsuresh@gmail.com>
2021-05-17 14:50:59 +0530

    Better name for function

--- kernel/arch/i386/display/image/draw.c
+++ kernel/arch/i386/display/image/draw.c
@@ -8,7 +8,7 @@ void *get_start_position()
     return (char *)ptr_to_y_pos() + skip_line(5);
 }
 
-int draw_row_of_pixel(void *dest, void *pixel_ptr, const int pixels_per_row)
+int copy_row_of_pixel(void *dest, void *pixel_ptr, const int pixels_per_row)
 {
     memcpy(dest, pixel_ptr, sizeof(uint32_t) * pixels_per_row);
     return pixels_per_row;
@@ -20,7 +20,7 @@ void draw_row_by_row(image_t image)
     char *write = get_start_position();
     for (int i = 0; i < image.height; ++i)
     {
-        read += draw_row_of_pixel(write, read, image.width);
+        read += copy_row_of_pixel(write, read, image.width);
         write += skip_line(1);
     }
 }
