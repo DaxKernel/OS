98210431206fe8161ab7729e73e1cff7b163f363
RMidhunSuresh <rmidhunsuresh@gmail.com>
2021-05-29 18:46:56 +0530

    Implement shutdown and panic

--- kernel/arch/i386/repl.c
+++ kernel/arch/i386/repl.c
@@ -4,7 +4,8 @@
 #include <stdbool.h>
 #include <string.h>
 #include <stdio.h>
-
+#include <kernel/io.h>
+#include <kernel/panic.h>
 void repl(){
     char command[20];
     tty_write_string("$root:");
@@ -13,6 +14,12 @@ void repl(){
         draw_image();
     else if(strcmp(command, "colortext") == 0){
         tty_print_rainbow("Hello from the world of DaxOS!");
+    }
+    else if(strcmp(command, "shutdown") == 0){
+        outportb(0x604, 0x2000);
+    }
+    else if(strcmp(command, "panic") == 0){
+        k_panic("This is a demo of a panic() in DaxOS.");
     }
     tty_insert_char('\n');
 }
