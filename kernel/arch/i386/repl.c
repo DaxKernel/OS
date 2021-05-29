a3a872ec5fc9ef64e1eb583afb0f1850746c9d5d
RMidhunSuresh <rmidhunsuresh@gmail.com>
2021-05-29 18:30:48 +0530

    Implement REPL loop

---
+++ kernel/arch/i386/repl.c
@@ -0,0 +1,26 @@
+#include <kernel/repl.h>
+#include <kernel/display/tty.h>
+#include <kernel/display/draw.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdio.h>
+
+void repl(){
+    char command[20];
+    tty_write_string("$root:");
+    scanf("%s", command);
+    if(strcmp(command, "nyancat") == 0)
+        draw_image();
+    else if(strcmp(command, "colortext") == 0){
+        tty_print_rainbow("Hello from the world of DaxOS!");
+    }
+    tty_insert_char('\n');
+    tty_insert_char('\n');
+}
+
+void accept_commands(){
+    while(true){
+        repl();
+        asm("hlt");
+    }
+}
---
+++ kernel/include/kernel/repl.h
@@ -0,0 +1,4 @@
+#ifndef REPL_H
+#define REPL_H
+void accept_commands();
+#endif
